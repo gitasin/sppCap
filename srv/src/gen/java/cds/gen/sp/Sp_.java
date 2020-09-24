ac40391ee044d71480f77b7646f9ac68225f630e
bhgu-lgcns <bhgu@lgcns.com>
2020-09-24 06:41:21 +0000

    테스트 Vendor Pool old

---
+++ srv/src/gen/java/cds/gen/sp/Sp_.java
@@ -0,0 +1,12 @@
+package cds.gen.sp;
+
+import com.sap.cds.ql.CdsName;
+import java.lang.Class;
+import java.lang.String;
+
+@CdsName("sp")
+public interface Sp_ {
+  String CDS_NAME = "sp";
+
+  Class<VendorPoolOld_> VENDOR_POOL_OLD = VendorPoolOld_.class;
+}
