726141498ff1b66d8dbea67e0931a217630097f6
ryan <ryan@mdpert.com>
2020-10-27 09:18:25 +0000

    add some feature for launcher

--- app/portal/controller/Launchpad.controller.js
+++ app/portal/controller/Launchpad.controller.js
@@ -1,13 +1,15 @@
 sap.ui.define([
+	"sap/ui/Device",
     "./BaseController",
     "sap/ui/model/json/JSONModel",
     "sap/m/MessageToast",
     "sap/m/MessageBox",
     "sap/ui/model/Filter",
     "sap/ui/model/FilterOperator",
-    "sap/uxap/ObjectPageSection"
+    "sap/uxap/ObjectPageSection",
+    "sap/ui/core/ComponentContainer"
 ],
-    function (BaseController, JSONModel, MessageToast, MessageBox, Filter, FilterOperator, ObjectPageSection) {
+    function (Device, BaseController, JSONModel, MessageToast, MessageBox, Filter, FilterOperator, ObjectPageSection, ComponentContainer) {
         "use strict";
 
         return BaseController.extend("spp.portal.controller.Launchpad", {
@@ -16,24 +18,84 @@ sap.ui.define([
                 var oModel = new JSONModel(sap.ui.require.toUrl("spp/portal/mockdata") + "/apps.json");
                 this.getView().setModel(oModel);
 
-                var oLaunchpadSections = this.getView().byId('launchpadSections');
-                if (oLaunchpadSections) {
-                    debugger;
-                    //oLaunchpadSections.setAggregation("sections", new ObjectPageSection());
-                }
+			    this._setToggleButtonTooltip(!Device.system.desktop);
+            },
 
+            onSideNavButtonPress: function(){
+                var oToolPage = this.byId("toolPage");
+                var bSideExpanded = oToolPage.getSideExpanded();
+                this._setToggleButtonTooltip(bSideExpanded);
+                oToolPage.setSideExpanded(!oToolPage.getSideExpanded());
+            },
+
+            _setToggleButtonTooltip: function (bLarge) {
+                var oToggleButton = this.byId('sideNavigationToggleButton');
+                if (bLarge) {
+                    oToggleButton.setTooltip('Large Size Navigation');
+                } else {
+                    oToggleButton.setTooltip('Small Size Navigation');
+                }
             },
 
             onCollapseExpandPress: function () {
                 var oNavigationList = this.byId("navigationList");
                 var bExpanded = oNavigationList.getExpanded();
-
                 oNavigationList.setExpanded(!bExpanded);
             },
 
             onHideShowSubItemPress: function () {
                 var oNavListItem = this.byId("subItemThree");
                 oNavListItem.setVisible(!oNavListItem.getVisible());
+            },
+
+            onMenuPress: function(oEvent){
+                var oTile = oEvent.getSource(),
+                    sPath = oEvent.getSource().getBindingContext().getPath(),
+                    oModel = this.getView().getModel(),
+                    oToolPage = this.getView().byId('toolPage'),
+                    sUrl = oModel.getProperty(sPath + '/url'),
+                    sComponent = oModel.getProperty(sPath + '/component');
+
+                oToolPage.removeMainContent(0);
+                oToolPage.addMainContent(new ComponentContainer({
+                    name: sComponent,
+                    async: true,
+                    url: sUrl
+                }));
+            },
+
+            onTilePress: function(oEvent){
+                // var oTile = oEvent.getSource(),
+                //     sPath = oEvent.getSource().getBindingContext().getPath(),
+                //     oModel = this.getView().getModel(),
+                //     oAppData = oModel.getProperty(sPath | '/app');
+                
+                // this.getView().byId('pageContainer').setVisible(false);
+
+                oToolPage.removeMainContent(0);
+                this.getView().byId('toolPage').addMainContent(new ComponentContainer({
+                    name: "cm.controlOptionMgr",
+                    async: true,
+                    url: "../cm/controlOptionMgr/webapp"
+                }));
+                
+            },
+            
+            onTilePress2: function(oEvent){
+                // var oTile = oEvent.getSource(),
+                //     sPath = oEvent.getSource().getBindingContext().getPath(),
+                //     oModel = this.getView().getModel(),
+                //     oAppData = oModel.getProperty(sPath | '/app');
+                
+                // this.getView().byId('pageContainer').setVisible(false);
+
+                this.getView().byId('toolPage').removeMainContent(0);
+                this.getView().byId('toolPage').addMainContent(new ComponentContainer({
+                    name: "cm.codeMgr",
+                    async: true,
+                    url: "../cm/codeMgr/webapp"
+                }));
+                
             }
 
         });
