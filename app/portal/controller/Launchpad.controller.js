da186f71a167690ab72e71cfcb13d189859a2d69
Ryan Moon <ryan@mdpert.com>
2020-10-28 09:50:48 +0900

    get launchpad more licely

--- app/portal/controller/Launchpad.controller.js
+++ app/portal/controller/Launchpad.controller.js
@@ -15,7 +15,16 @@ sap.ui.define([
         return BaseController.extend("spp.portal.controller.Launchpad", {
 
             onInit: function () {
-                var oModel = new JSONModel(sap.ui.require.toUrl("spp/portal/mockdata") + "/apps.json");
+                var oMenuModel = new JSONModel(sap.ui.require.toUrl("spp/portal/mockdata") + "/menu.json");
+                this.getView().setModel(oMenuModel, "menu");
+
+                var me = this;
+                oMenuModel.dataLoaded().then(function(){
+                    var oTab = me.byId("toolPageTabHeader");
+                    oTab.setSelectedKey('cm');
+                });
+
+                var oModel = new JSONModel(sap.ui.require.toUrl("spp/portal/mockdata") + "/apps-cm.json");
                 this.getView().setModel(oModel);
 
 			    this._setToggleButtonTooltip(!Device.system.desktop);
@@ -29,11 +38,11 @@ sap.ui.define([
             },
 
             _setToggleButtonTooltip: function (bLarge) {
-                var oToggleButton = this.byId('sideNavigationToggleButton');
+                var oToggleButton = this.byId("sideNavigationToggleButton");
                 if (bLarge) {
-                    oToggleButton.setTooltip('Large Size Navigation');
+                    oToggleButton.setTooltip("Large Size Navigation");
                 } else {
-                    oToggleButton.setTooltip('Small Size Navigation');
+                    oToggleButton.setTooltip("Small Size Navigation");
                 }
             },
 
@@ -49,12 +58,28 @@ sap.ui.define([
             },
 
             onMenuPress: function(oEvent){
+                var sText = oEvent.getParameter("item").getProperty("text"),
+                    oData = this.getView().getModel("menu").getData(),
+                    id = "cm";
+
+                jQuery.each(oData, function(i, v){
+                    if(v.name == sText){
+                        id = v.id;
+                        return false;
+                    }
+                });
+
+                var oModel = new JSONModel(sap.ui.require.toUrl("spp/portal/mockdata") + "/apps-"+id+".json");
+                this.getView().setModel(oModel);
+            },
+
+            onAppPress: function(oEvent){
                 var oTile = oEvent.getSource(),
                     sPath = oEvent.getSource().getBindingContext().getPath(),
                     oModel = this.getView().getModel(),
-                    oToolPage = this.getView().byId('toolPage'),
-                    sUrl = oModel.getProperty(sPath + '/url'),
-                    sComponent = oModel.getProperty(sPath + '/component');
+                    oToolPage = this.byId("toolPage"),
+                    sUrl = oModel.getProperty(sPath + "/url"),
+                    sComponent = oModel.getProperty(sPath + "/component");
 
                 oToolPage.removeMainContent(0);
                 oToolPage.addMainContent(new ComponentContainer({
@@ -68,34 +93,16 @@ sap.ui.define([
                 // var oTile = oEvent.getSource(),
                 //     sPath = oEvent.getSource().getBindingContext().getPath(),
                 //     oModel = this.getView().getModel(),
-                //     oAppData = oModel.getProperty(sPath | '/app');
+                //     oAppData = oModel.getProperty(sPath | "/app");
                 
-                // this.getView().byId('pageContainer').setVisible(false);
+                // this.byId("pageContainer").setVisible(false);
 
                 oToolPage.removeMainContent(0);
-                this.getView().byId('toolPage').addMainContent(new ComponentContainer({
+                this.byId("toolPage").addMainContent(new ComponentContainer({
                     name: "cm.controlOptionMgr",
                     async: true,
                     url: "../cm/controlOptionMgr/webapp"
                 }));
-                
-            },
-            
-            onTilePress2: function(oEvent){
-                // var oTile = oEvent.getSource(),
-                //     sPath = oEvent.getSource().getBindingContext().getPath(),
-                //     oModel = this.getView().getModel(),
-                //     oAppData = oModel.getProperty(sPath | '/app');
-                
-                // this.getView().byId('pageContainer').setVisible(false);
-
-                this.getView().byId('toolPage').removeMainContent(0);
-                this.getView().byId('toolPage').addMainContent(new ComponentContainer({
-                    name: "cm.codeMgr",
-                    async: true,
-                    url: "../cm/codeMgr/webapp"
-                }));
-                
             }
 
         });
