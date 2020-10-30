166d11b05806a632fd84c9c49c68d571c2d34b3b
KT Kim <titi112>
2020-10-30 08:57:16 +0000

    msgMgr

---
+++ app/cm/msgMgr/webapp/model/formatter.js
@@ -0,0 +1,31 @@
+sap.ui.define([], function () {
+	"use strict";
+	return {
+
+        setEnable : function (oValue) {
+            
+            //var oType = new sap.ui.model.type.Boolean();           
+            return false ;          
+        },
+
+        setStatus : function (oValue, oEntity, oEvent) {
+            
+            switch (oEvent)
+            {
+              case "" :
+               return false;
+              case "AddRow" :
+                  switch (oEntity)     // 전달되는 값이 없을경우(저장되지 않은 데이터)  수정가능 ..
+                    {
+                    case "" :
+                        return true;
+                    default :
+                        return false;
+                    } ;
+               default :
+                return false;
+                  
+            }      
+        }
+    };
+});
